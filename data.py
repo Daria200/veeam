b84ddcceacc2a1f3bf84ff11268fd281101fe395
Daria Neznaeva <darianeznaeva@gmail.com>
2022-09-30 20:26:44 +0200

    creating function check files and create report

---
+++ data.py
@@ -0,0 +1,3 @@
+path_to_source = "./source"
+path_to_replica = "./replica"
+author = "Me"
--- synchronizer.py
+++ synchronizer.py
@@ -4,42 +4,48 @@ import shutil
 import csv
 from datetime import date
 
-path_to_source = "./source"
-path_to_replica = "./replica"
-author = "Me"
 
-files_in_source = os.listdir(path_to_source)
-files_in_replica = os.listdir(path_to_replica)
-
-changed_files = []
+path_to_source = input("Path to the source folder: ")
+path_to_replica = input("Path to the replica folder: ")
+author = input("Your name: ")
 
 
-for file in files_in_source:
-    if file not in files_in_replica:
-        shutil.copyfile(f"{path_to_source}/{file}", f"{path_to_replica}/{file}")
-        print(f"File {file} was copied to the replica folder")
-        changed_files.append([date.today(), author, "creation", file])
+def check_files_and_create_report(path_to_source, path_to_replica, author):
+    files_in_source = os.listdir(path_to_source)
+    files_in_replica = os.listdir(path_to_replica)
 
-for file in files_in_replica:
-    if file not in files_in_source:
-        os.remove(f"{path_to_replica}/{file}")
-        print(f"File {file} was deleted in the replica folder")
-        changed_files.append([date.today(), author, "deletion", file])
+    changed_files = []
 
-files_in_replica = os.listdir(path_to_replica)
-files_in_source = os.listdir(path_to_source)
+    for file in files_in_source:
+        if file not in files_in_replica:
+            shutil.copyfile(f"{path_to_source}/{file}", f"{path_to_replica}/{file}")
+            print(f"File {file} was copied to the replica folder")
+            changed_files.append([date.today(), author, "creation", file])
 
-headers = ["Date", "Author", "Action", "File name"]
+    for file in files_in_replica:
+        if file not in files_in_source:
+            os.remove(f"{path_to_replica}/{file}")
+            print(f"File {file} was deleted in the replica folder")
+            changed_files.append([date.today(), author, "deletion", file])
 
-files_in_directory = os.listdir(".")
-index = files_in_directory.count("report.csv")
-if f"report{index}.csv" in files_in_directory:
-    while f"report{index}.csv" in files_in_directory:
-        index + 1
+    files_in_replica = os.listdir(path_to_replica)
+    files_in_source = os.listdir(path_to_source)
+
+    headers = ["Date", "Author", "Action", "File name"]
+
+    files_in_directory = os.listdir(".")
+    index = ""
+    if files_in_directory.count("report.csv") > 0:
+        index = files_in_directory.count("report.csv")
+    if f"reports/report{index}.csv" in files_in_directory:
+        while f"report{index}.csv" in files_in_directory:
+            index + 1
+
+    with open(f"report{index}.csv", "w", newline="") as report:
+        writer = csv.writer(report)
+        writer.writerow(headers)
+        for row in changed_files:
+            writer.writerow(row)
 
 
-with open(f"report{index}.csv", "w", newline="") as report:
-    writer = csv.writer(report)
-    writer.writerow(headers)
-    for row in changed_files:
-        writer.writerow(row)
+check_files_and_create_report(path_to_source, path_to_replica, author)
